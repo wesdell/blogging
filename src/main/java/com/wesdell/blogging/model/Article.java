3b1d43424af362b9e17aa20a388264e98c7a3d77
Wesdell <wesdell17@gmail.com>
2025-05-13 18:06:42 -0500

    New article entity

---
+++ src/main/java/com/wesdell/blogging/model/Article.java
@@ -0,0 +1,74 @@
+package com.wesdell.blogging.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+    private String author;
+    private LocalDate publishedAt;
+
+    public Article() {
+    }
+
+    public Article(Long id, String title, String content, String author, LocalDate publishedAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.publishedAt = publishedAt;
+    }
+
+    public Article(String title, String content, String author, LocalDate publishedAt) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.publishedAt = publishedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(LocalDate publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+}
