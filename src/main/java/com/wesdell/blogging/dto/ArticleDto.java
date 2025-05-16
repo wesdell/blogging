54aa12a7ad022c920991ece512308092280e5cb6
Wesdell <wesdell17@gmail.com>
2025-05-15 19:54:39 -0500

    New dto for Article entity

---
+++ src/main/java/com/wesdell/blogging/dto/ArticleDto.java
@@ -0,0 +1,51 @@
+package com.wesdell.blogging.dto;
+
+import java.time.LocalDate;
+
+public class ArticleDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+    private LocalDate publishedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(LocalDate publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+}
