59eccf0f83c743cde5e9eed92f31defd2f0a6d15
Wesdell <wesdell17@gmail.com>
2025-05-16 17:35:04 -0500

    New mappers to map from an updated and created Article dto to an Article entity

--- src/main/java/com/wesdell/blogging/mapper/ArticleMapper.java
+++ src/main/java/com/wesdell/blogging/mapper/ArticleMapper.java
@@ -1,10 +1,14 @@
 package com.wesdell.blogging.mapper;
 
 import com.wesdell.blogging.dto.ArticleDto;
+import com.wesdell.blogging.dto.CreateArticleDto;
+import com.wesdell.blogging.dto.UpdateArticleDto;
 import com.wesdell.blogging.model.Article;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ArticleMapper {
     ArticleDto toDto(Article article);
+    Article toEntity(CreateArticleDto dto);
+    Article toEntity(UpdateArticleDto dto);
 }
